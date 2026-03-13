6d57b02e3592aae28103cf0687f3ee8cd40caa50
Steeven24 <loorjins@gmail.com>
2026-03-13 01:38:51 -0500

    add main screen UI with initial setup for MainWindow class

---
+++ ui/main_screen.py
@@ -0,0 +1,13 @@
+from PySide6.QtWidgets import QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget, QGridLayout
+
+class MainWindow(QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("Test de Comprension Lectora con IA")
+        self.setGeometry(100, 100, 1080, 720)
+        
+        
+        
+        
